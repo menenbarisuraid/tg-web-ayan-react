a92fd2cbb40b633ec28b1e179980377230e25998
menenbarisuraid <nesuraisyn04@gmail.com>
2024-07-25 11:41:19 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React, { useState } from 'react';
-import { Container, AppBar, Tabs, Tab, Box } from '@mui/material';
+import { Container, Box, BottomNavigation, BottomNavigationAction } from '@mui/material';
 import Main from './components/Main';
 import Employees from './components/Employees';
 import Profile from './components/Profile';
+import HomeIcon from '@mui/icons-material/Home';
+import PeopleIcon from '@mui/icons-material/People';
+import PersonIcon from '@mui/icons-material/Person';
 
 const App = () => {
   const [value, setValue] = useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
   return (
     <Container>
-      <AppBar position="static" color="default">
-        <Tabs value={value} onChange={handleChange} variant="fullWidth">
-          <Tab label="Главная" />
-          <Tab label="Сотрудники" />
-          <Tab label="Профиль" />
-        </Tabs>
-      </AppBar>
       <Box mt={2}>
         {value === 0 && <Main />}
         {value === 1 && <Employees />}
         {value === 2 && <Profile />}
       </Box>
+      <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        showLabels
+      >
+        <BottomNavigationAction label="Главная" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Сотрудники" icon={<PeopleIcon />} />
+        <BottomNavigationAction label="Профиль" icon={<PersonIcon />} />
+      </BottomNavigation>
     </Container>
   );
 };
